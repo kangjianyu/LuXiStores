bcc5e7d91f60d677136b1755f0b575cbbbc3343f
康镇春 <kangzhenchun@inke.cn>
2019-05-25 09:23:45 +0800

    优化redis

--- user/dao/redis.go
+++ user/dao/redis.go
@@ -6,24 +6,28 @@ import (
 	"time"
 )
 
-//GetUserCookie 获取用户token
-func GetUserToken(key string) (value string, err error) {
-	result := common.RedisClient.Get(key)
-	err = result.Err()
+var Rds iRedis = redisImpl{}
+
+type iRedis interface {
+	GetUserToken(key string) (value string, err error)
+	SetUserToken(key string, value string, TTl time.Duration) (err error)
+	DelUserToken(key string) (err error)
+}
+
+type redisImpl struct{}
+
+func (redisImpl) GetUserToken(key string) (value string, err error) {
+	value, err = common.RedisClient.Get(key).Result()
 	if err != nil {
 		log.Error("get cookie error:%v in redis key:%s", err, key)
 		return
 	}
-	value = result.Val()
 	log.Info("get cookie in redis key:%s,value:%s", key, err)
 	return
 }
 
-//SetValueByRedis 用户设置token
-func SetUserToken(key string, value string, TTl time.Duration) (valid string, err error) {
-	result := common.RedisClient.Set(key, value, TTl*time.Second)
-	err = result.Err()
-	valid = result.Val()
+func (redisImpl) SetUserToken(key string, value string, TTl time.Duration) (err error) {
+	_, err = common.RedisClient.Set(key, value, TTl*time.Second).Result()
 	if err != nil {
 		log.Error("set cookie error:%v in redis key:%s,value:%s", err, key, value)
 		return
@@ -32,11 +36,8 @@ func SetUserToken(key string, value string, TTl time.Duration) (valid string, er
 	return
 }
 
-//DelUserToken 删除用户token
-func DelUserToken(key string) (valid int64, err error) {
-	result := common.RedisClient.Del(key)
-	valid = result.Val()
-	err = result.Err()
+func (redisImpl) DelUserToken(key string) (err error) {
+	_, err = common.RedisClient.Del(key).Result()
 	if err != nil {
 		log.Error("del token error:%v in redis key:%s", err, key)
 		return
